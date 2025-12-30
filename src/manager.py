fd659ee64ccdc20d15e6da9e81ad25dfb8850caa
David Marom <davidmarom@hotmail.com>
2025-12-30 21:03:51 +0200

    lint

--- src/manager.py
+++ src/manager.py
@@ -7,78 +7,92 @@ from analysis.named_analyzer import NamedAnalyzer
 from analysis.analyzers.records_count_analyzer import RecordsCountAnalyzer
 from analysis.analyzers.average_words_length_analyzer import AverageWordsLengthAnalyzer
 from analysis.analyzers.most_common_word_analyzer import MostCommonWordsAnalyzer
-from analysis.analyzers.top_longest_text_records_analyzer import TopLongestTextRecordsAnalyzer
+from analysis.analyzers.top_longest_text_records_analyzer import (
+    TopLongestTextRecordsAnalyzer,
+)
 from analysis.analyzers.uppercase_word_count_analyzer import UppercaseWordCountAnalyzer
 
 from results_transformer import ResultsTransformer
 
-from report_builder import ReportBuilder,JsonReportBuilder
+from report_builder import ReportBuilder, JsonReportBuilder
 
 TARGET_CATEGORY_FEATURE = "Biased"
 TARGET_TEXT_FEATURE = "Text"
 NUM_COMMON_WORDS = 10
 NUM_LONGEST_TWEETS = 3
-DEFAULT_CATEGORY_NAME = 'total'
+DEFAULT_CATEGORY_NAME = "total"
 
-CATEGORY_MAPPING = {0: 'non_antisemitic', 1: 'antisemitic'}
+CATEGORY_MAPPING = {0: "non_antisemitic", 1: "antisemitic"}
 
-DATASET_FILEPATH = 'data/tweets_dataset.csv'
-CLEANED_DATASET_FILEPATH = 'results/tweets_dataset_cleaned.csv'
+DATASET_FILEPATH = "data/tweets_dataset.csv"
+CLEANED_DATASET_FILEPATH = "results/tweets_dataset_cleaned.csv"
 JSON_REPORT_FILEPATH = "results/results"
 
 REPORT_HEADLINES = {
-    'tt':"total_tweets",
-    'al':"average_length",
-    'cw':"common_words",
-    'lnt':f"longest_{NUM_LONGEST_TWEETS}_tweets",
-    'uw':"uppercase_words",
+    "tt": "total_tweets",
+    "al": "average_length",
+    "cw": "common_words",
+    "lnt": f"longest_{NUM_LONGEST_TWEETS}_tweets",
+    "uw": "uppercase_words",
 }
 
 # Load dataset
-loader = CSVTextDataSetLoader(filepath=DATASET_FILEPATH,
-                              chosen_features=[TARGET_TEXT_FEATURE,TARGET_CATEGORY_FEATURE])
+loader = CSVTextDataSetLoader(
+    filepath=DATASET_FILEPATH,
+    chosen_features=[TARGET_TEXT_FEATURE, TARGET_CATEGORY_FEATURE],
+)
 dataset = loader.load()
 
 # Question 1 - Data Analysis
 analyzers = [
     NamedAnalyzer(
-        REPORT_HEADLINES['tt'], 
-        RecordsCountAnalyzer(dataset, 
-                             category_feature=TARGET_CATEGORY_FEATURE, 
-                             default_category=DEFAULT_CATEGORY_NAME)
+        REPORT_HEADLINES["tt"],
+        RecordsCountAnalyzer(
+            dataset,
+            category_feature=TARGET_CATEGORY_FEATURE,
+            default_category=DEFAULT_CATEGORY_NAME,
         ),
+    ),
     NamedAnalyzer(
-        REPORT_HEADLINES['al'], 
-        AverageWordsLengthAnalyzer(dataset, 
-                                   text_feature=TARGET_TEXT_FEATURE, 
-                                   category_feature=TARGET_CATEGORY_FEATURE,
-                                   default_category=DEFAULT_CATEGORY_NAME,
-                                   dropna=True)
+        REPORT_HEADLINES["al"],
+        AverageWordsLengthAnalyzer(
+            dataset,
+            text_feature=TARGET_TEXT_FEATURE,
+            category_feature=TARGET_CATEGORY_FEATURE,
+            default_category=DEFAULT_CATEGORY_NAME,
+            dropna=True,
         ),
+    ),
     NamedAnalyzer(
-        REPORT_HEADLINES['cw'], 
-        MostCommonWordsAnalyzer(dataset, 
-                                text_feature=TARGET_TEXT_FEATURE, 
-                                top_n=NUM_COMMON_WORDS,
-                                default_category=DEFAULT_CATEGORY_NAME)
+        REPORT_HEADLINES["cw"],
+        MostCommonWordsAnalyzer(
+            dataset,
+            text_feature=TARGET_TEXT_FEATURE,
+            top_n=NUM_COMMON_WORDS,
+            default_category=DEFAULT_CATEGORY_NAME,
         ),
+    ),
     NamedAnalyzer(
-        REPORT_HEADLINES['lnt'], 
-        TopLongestTextRecordsAnalyzer(dataset, 
-                                      text_feature=TARGET_TEXT_FEATURE, 
-                                      top_n=NUM_LONGEST_TWEETS, 
-                                      category_feature=TARGET_CATEGORY_FEATURE,
-                                      default_category=DEFAULT_CATEGORY_NAME,
-                                      dropna=True)
+        REPORT_HEADLINES["lnt"],
+        TopLongestTextRecordsAnalyzer(
+            dataset,
+            text_feature=TARGET_TEXT_FEATURE,
+            top_n=NUM_LONGEST_TWEETS,
+            category_feature=TARGET_CATEGORY_FEATURE,
+            default_category=DEFAULT_CATEGORY_NAME,
+            dropna=True,
         ),
+    ),
     NamedAnalyzer(
-        REPORT_HEADLINES['uw'], 
-        UppercaseWordCountAnalyzer(dataset, 
-                                   text_feature=TARGET_TEXT_FEATURE, 
-                                   category_feature=TARGET_CATEGORY_FEATURE,
-                                   default_category=DEFAULT_CATEGORY_NAME,
-                                   dropna=True)
-        )
+        REPORT_HEADLINES["uw"],
+        UppercaseWordCountAnalyzer(
+            dataset,
+            text_feature=TARGET_TEXT_FEATURE,
+            category_feature=TARGET_CATEGORY_FEATURE,
+            default_category=DEFAULT_CATEGORY_NAME,
+            dropna=True,
+        ),
+    ),
 ]
 
 composite_analyzer = CompositeAnalyzer(analyzers)
@@ -92,14 +106,16 @@ clean_df.to_csv(CLEANED_DATASET_FILEPATH, index=False)
 # Question 3 - Build Report
 report_transformer = ResultsTransformer(
     category_mapping=CATEGORY_MAPPING,
-    tuple_extract_keys=[REPORT_HEADLINES['cw']],
-    tuple_extract_index = 0,
+    tuple_extract_keys=[REPORT_HEADLINES["cw"]],
+    tuple_extract_index=0,
     exclude_keys={
-        REPORT_HEADLINES['lnt']: [DEFAULT_CATEGORY_NAME],
-        }
+        REPORT_HEADLINES["lnt"]: [DEFAULT_CATEGORY_NAME],
+    },
 )
 final_results = report_transformer.transform(results)
 
-report_builder:ReportBuilder = JsonReportBuilder(filepath=JSON_REPORT_FILEPATH, report=final_results)
+report_builder: ReportBuilder = JsonReportBuilder(
+    filepath=JSON_REPORT_FILEPATH, report=final_results
+)
 
 report_builder.save()
